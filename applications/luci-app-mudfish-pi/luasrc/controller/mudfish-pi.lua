e082a80ec372097c1c9157193c035265d1682753
Weongyo Jeong <chloe0528@nate.com>
2016-11-09 16:20:05 +0900

    .

---
+++ applications/luci-app-mudfish-pi/luasrc/controller/mudfish-pi.lua
@@ -0,0 +1,11 @@
+-- Copyright 2008 Steven Barth <steven@midlink.org>
+-- Copyright 2008 Jo-Philipp Wich <jow@openwrt.org>
+-- Licensed to the public under the Apache License 2.0.
+
+module("luci.controller.mudfish-pi", package.seeall)
+
+function index()
+	entry( {"admin", "services", "mudfish-pi"}, cbi("mudfish-pi"), _("Mudfish") )
+	entry( {"admin", "services", "mudfish-pi", "basic"},    cbi("mudfish-pi-basic"),    nil ).leaf = true
+	entry( {"admin", "services", "mudfish-pi", "advanced"}, cbi("mudfish-pi-advanced"), nil ).leaf = true
+end
